54d78a44fda6b3db02931f9a5e013a23fa8ba164
Tanya <tanya.butova@gmail.com>
2021-11-16 20:21:15 +0500

    updated solution

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,67 +1,72 @@
 package ru.netology;
+import com.github.javafaker.Faker;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-
 import static io.restassured.RestAssured.given;
+import lombok.Value;
+
+
+import java.util.Locale;
 
 public class DataGenerator {
-    static void sendForRegistration(PersonGenerator person) {
-        RequestSpecification requestSpec = new RequestSpecBuilder()
-                .setBaseUri("http://localhost")
-                .setPort(9999)
-                .setAccept(ContentType.JSON)
-                .setContentType(ContentType.JSON)
-                .log(LogDetail.ALL)
-                .build();
+    public DataGenerator() {
+    }
+    // Создаем дата-класс пользователя, а также при помощи Lombok автоматически создаем конструктор
+    // на основе этого класса будут созданы пользователи с разным статусом (через конструктор напрямую инициилизируем поля)
 
-        given()
+    @Value
+    public static class UserInfo {
+        String login;
+        String password;
+        String status;
+    }
+
+    // Создадим объект класса faker c заданной локалью
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    // Универсальные методы генерации данных (логина и пароля),а для этого перед этим создадим объект класса faker c заданной локалью
+    public static String getLogin (){
+        return faker.name().username();
+    }
+
+    public static String getPassword (){
+        return faker.internet().password();
+    }
+
+    // Создаем активного пользователя
+    public static UserInfo createActiveUser(){
+        UserInfo user = new UserInfo(getLogin(),getPassword(),"active");
+        sendRequestToCreateUser (user); // отправь логин,пароль и статус в тело запроса
+        return user; // эти же данные используй в тесте
+    }
+
+    // Создаем заблокированного пользователя
+    public static UserInfo createBlockedUser (){
+        UserInfo user = new UserInfo(getLogin(),getPassword(),"blocked");
+        sendRequestToCreateUser (user);  // отправь логин,пароль и статус в тело запроса
+        return user; // эти же данные используй в тесте
+    }
+
+    // Данные пользователей теперь необходимо передать в тело запроса. Для этого создадим класс, который отправляет запросы к SUT
+
+    // Спецификация, которая используется при отправке запроса (указывается в .spec)
+    private static RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri("http://localhost")
+            .setPort(9999)
+            .setAccept(ContentType.JSON)
+            .setContentType(ContentType.JSON)
+            .log(LogDetail.ALL)
+            .build();
+
+    public static void sendRequestToCreateUser(UserInfo user) {
+        given() // "дано"
                 .spec(requestSpec) // указываем, какую спецификацию используем
-                .body(person) // передаём в теле объект, который будет преобразован в JSON
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
                 .when() // "когда"
                 .post("/api/system/users") // на какой путь, относительно BaseUri отправляем запрос
                 .then() // "тогда ожидаем"
                 .statusCode(200); // код 200 OK
-    }
-
-    static PersonGenerator createWithoutRegistration(String locale) {
-        PersonGenerator person = new PersonGenerator(locale);
-        person.generatePersonWithValidPassword();
-        return person;
-    }
-
-    static PersonGenerator getPersonWithInvalidLogin(String locale) {
-        PersonGenerator person = new PersonGenerator(locale);
-        person.generatePersonWithValidPassword();
-        DataGenerator.sendForRegistration(person);
-        PersonGenerator person2 = new PersonGenerator(locale);
-        person2.generatePersonWithValidPassword();
-        person2.setPassword(person.getPassword());
-        return person2;
-    }
-
-    static PersonGenerator getPersonWithInvalidPassword(String locale) {
-        PersonGenerator person = new PersonGenerator(locale);
-        person.generatePersonWithValidPassword();
-        DataGenerator.sendForRegistration(person);
-        PersonGenerator person2 = new PersonGenerator(locale);
-        person2.generatePersonWithValidPassword();
-        person2.setLogin(person.getLogin());
-        return person2;
-    }
-
-    static PersonGenerator getPersonWithBlockedStatus(String locale) {
-        PersonGenerator person = new PersonGenerator(locale);
-        person.generatePersonWithBlockedStatus();
-        DataGenerator.sendForRegistration(person);
-        return person;
-    }
-
-    static PersonGenerator getValidPerson(String locale) {
-        PersonGenerator person = new PersonGenerator(locale);
-        person.generatePersonWithValidPassword();
-        DataGenerator.sendForRegistration(person);
-        return person;
     }
 }
--- src/test/java/ru/netology/IBankTest.java
+++ src/test/java/ru/netology/IBankTest.java
@@ -1,77 +1,96 @@
 package ru.netology;
-import com.codeborne.selenide.Condition;
-import lombok.val;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static ru.netology.DataGenerator.*;
 
 public class IBankTest {
-    @Test
-    void shouldValidPersonTest() {
-        PersonGenerator person = DataGenerator.getValidPerson("en");
+    @BeforeEach
+    void setUp() {
+        Configuration.browser = "chrome";
+        //Открытие формы
         open("http://localhost:9999");
-        $("[data-test-id=login] .input__control").setValue(person.getLogin());
-        $("[data-test-id=password] .input__control").setValue(person.getPassword());
+    }
+    // 1 - Наличие активного пользователя (успешный вход)
+    @Test
+    public void shouldLoginRegisteredUser() {
+        DataGenerator.UserInfo activeUser = createActiveUser();
+        $("[data-test-id=login] .input__control").setValue(activeUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(activeUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $(".heading").shouldHave(Condition.exactText("Личный кабинет"));
+        $(Selectors.withText("Личный кабинет")).shouldBe(visible);
     }
 
+    // 2 - Наличие заблокированного пользователя
     @Test
-    void shouldInvalidLoginTest() {
-        PersonGenerator person = DataGenerator.getPersonWithInvalidLogin("en");
-        open("http://localhost:9999");
-        $("[data-test-id=login] .input__control").setValue(person.getLogin());
-        $("[data-test-id=password] .input__control").setValue(person.getPassword());
+    public void shouldGetErrorIfUserIsBlocked() {
+        DataGenerator.UserInfo blockedUser = createBlockedUser();
+        $("[data-test-id=login] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(blockedUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("Ошибка!"));
+        $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Пользователь заблокирован"));
     }
 
+    // 3 - Отсутствие пользователя
     @Test
-    void shouldInvalidPasswordTest() {
-        PersonGenerator person = DataGenerator.getPersonWithInvalidPassword("en");
-        open("http://localhost:9999");
-        $("[data-test-id=login] .input__control").setValue(person.getLogin());
-        $("[data-test-id=password] .input__control").setValue(person.getPassword());
+    public void shouldGetErrorIfUserDoesNotExist() {
+        $("[data-test-id=login] .input__control").setValue(getLogin());
+        $("[data-test-id=password] .input__control").setValue(getPassword());
         $("[data-test-id=action-login]").click();
-        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("Ошибка!"));
+        $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
     }
 
+    // 4 - Невалидный логин активного пользователя
     @Test
-    void shouldBlockedStatusTest() {
-        val person = DataGenerator.getPersonWithBlockedStatus("en");
-        open("http://localhost:9999");
-        $("[data-test-id=login] .input__control").setValue(person.getLogin());
-        $("[data-test-id=password] .input__control").setValue(person.getPassword());
+    public void shouldGetErrorIfActiveUserWithInvalidLogin() {
+        DataGenerator.UserInfo activeUser = createActiveUser();
+        $("[data-test-id=login] .input__control").setValue(getLogin());
+        $("[data-test-id=password] .input__control").setValue(activeUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("заблокирован"));
+        $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
     }
 
+    // 5 - Невалидный пароль активного пользователя
     @Test
-    void shouldNotExistPersonTest() {
-        PersonGenerator person = DataGenerator.createWithoutRegistration("en");
-        open("http://localhost:9999");
-        $("[data-test-id=login] .input__control").setValue(person.getLogin());
-        $("[data-test-id=password] .input__control").setValue(person.getPassword());
+    public void shouldGetErrorIfActiveUserWithInvalidPassword() {
+        DataGenerator.UserInfo activeUser = createActiveUser();
+        $("[data-test-id=login] .input__control").setValue(activeUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(getPassword());
         $("[data-test-id=action-login]").click();
-        $("[data-test-id=error-notification] .notification__content").shouldHave(Condition.text("Ошибка!"));
+        $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
     }
 
+    // 6 - Невалидный логин заблокированного пользователя
     @Test
-    void shouldWithoutPasswordTest() {
-        PersonGenerator person = DataGenerator.getValidPerson("en");
-        open("http://localhost:9999");
-        $("[data-test-id=login] .input__control").setValue(person.getLogin());
+    public void shouldGetErrorIfBlockedUserWithInvalidLogin() {
+        DataGenerator.UserInfo blockedUser = createBlockedUser();
+        $("[data-test-id=login] .input__control").setValue(getLogin());
+        $("[data-test-id=password] .input__control").setValue(blockedUser.getPassword());
         $("[data-test-id=action-login]").click();
-        $("[data-test-id=password] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
     }
 
+    // 7 - Невалидный пароль заблокированного пользователя
     @Test
-    void shouldWithoutLoginTest() {
-        PersonGenerator person = DataGenerator.getValidPerson("en");
-        open("http://localhost:9999");
-        $("[data-test-id=password] .input__control").setValue(person.getPassword());
+    public void shouldGetErrorIfBlockedUserWithInvalidPassword() {
+        DataGenerator.UserInfo blockedUser = createBlockedUser();
+        $("[data-test-id=login] .input__control").setValue(blockedUser.getLogin());
+        $("[data-test-id=password] .input__control").setValue(getPassword());
         $("[data-test-id=action-login]").click();
-        $("[data-test-id=login] .input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+        $("[data-test-id=error-notification]").shouldBe(visible).find(".notification__title").shouldHave(exactText("Ошибка"));
+        $("[data-test-id=error-notification] .notification__content").shouldHave(exactText("Ошибка! " + "Неверно указан логин или пароль"));
     }
+
 }
